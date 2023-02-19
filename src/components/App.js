12c309ff1fe18b39b128a29faddb56dffb8399a9
vrushabh1998 <119606803+vrushabh1998@users.noreply.github.com>
2023-02-19 14:32:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,22 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
   
-  const submitButton = () => {
-  console.log("form submitted");
+  const buttonClick=(event)=>{
+    event.preventDefault();
+   console.log("form submitted")
   }
+  return(
+    <div id ="main">
+    <form>
+    <label htmlFor='name'>Name</label>
+    <input id="name" type={"text"}/>
 
-
-  return (
-    <div id="main">
-      <form onSubmit={(event)
-        =>{event.preventDefault()}}
->
-  
-        <label htmlFor='name'>Name</label>
-        <input id="name" type={"text"} />
-
-        <br />
-        <br />
-        <button type='submit'onClick={submitButton}>Submit</button>
-      </form>
-    </div>
-  )
+    <br />
+    <br />
+    <button type='submit' onClick={buttonClick}>Submit</button>
+   </form>
+</div>
+)
 }
-
 
 export default App;
